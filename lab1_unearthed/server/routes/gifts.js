e5fd66782f32cfddf2a062181f53b428a5896951
YevheniyaSolomyana <72937387+YevheniyaSolomyana@users.noreply.github.com>
2023-09-27 10:00:05 -0400

    uploading lab2

--- lab1_unearthed/server/routes/gifts.js
+++ lab1_unearthed/server/routes/gifts.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import giftData from '../data/gifts.js'
+// import giftData from '../data/gifts.js' => don't need this anymore since we are serving tehe data from the database instead of directly from this file
+import GiftsController from '../controllers/giftsCRUD.js'
 
 
 const __filename = fileURLToPath(import.meta.url)
@@ -9,9 +10,10 @@ const __dirname = path.dirname(__filename)
 
 const giftsRouter = express.Router()
 
-giftsRouter.get('/', (req, res) => {
-    res.status(200).json(giftData)
-})
+// giftsRouter.get('/', (req, res) => {
+//     res.status(200).json(giftData)
+// })
+giftsRouter.get('/', GiftsController.getGifts)
 
 giftsRouter.get('/:giftId', (req, res) => {
     res.status(200).sendFile(path.resolve(__dirname, '../public/gift.html'))
